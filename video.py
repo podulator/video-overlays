55fbc9e472919eb87e653b872ae02974ac95f3b1
Mat Rowlands <mat.rowlands@justgiving.com>
2016-01-25 16:16:28 +0000

    updating the main script to test the libs

--- video.py
+++ video.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# requires
+# requires 
 # urllib3, python-pip
 # pip install boto BeautifulSoup
 
@@ -10,37 +10,59 @@ from BeautifulSoup import BeautifulSoup
 from BeautifulSoup import BeautifulStoneSoup
 import logging
 import sys
-import HTMLParser
-import re
+
+from FFMPEG import DrawText
+
 
 # logging setup
 
-debugLevel = logging.DEBUG#INFO
 logger = logging.getLogger('Video Processor')
-logger.setLevel(debugLevel)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch = logging.StreamHandler(sys.stdout)
 ch.setFormatter(formatter)
-ch.setLevel(debugLevel)
 logger.addHandler(ch)
 
 def setDebugLevel(val):
     logger.setLevel(val)
     ch.setLevel(val)
 
+debugLevel = logging.INFO
+debugLevel = logging.DEBUG
+setDebugLevel(debugLevel)
+
 # end logging setup
-
-
-html_parser = HTMLParser.HTMLParser()
-cleaner = re.compile("&rsquo;")
 
 def HTMLEntitiesToUnicode(text):
     """Converts HTML entities to unicode.  For example '&amp;' becomes '&'."""
     text = unicode(BeautifulStoneSoup(text, convertEntities=BeautifulStoneSoup.HTML_ENTITIES))
     return text.encode("utf-8")
 
-with open('PageReportV2.csv', 'rb') as f:
-    reader = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-    for row in reader:
-        row[4] = HTMLEntitiesToUnicode(BeautifulSoup(row[4]).getText())
-        logger.debug("csv parser :: {0}".format(row[4]))
+def LoadTokenList(data_file):
+    with open(data_file, 'rb') as f:
+        rows = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        tokens = rows.next()
+        logger.debug("data definitions :: {0}".format( " :: ".join( map( str, tokens ) ) ) )
+        return tokens
+
+def CsvDataIterator(data_file):
+    with open(data_file, 'rb') as f:
+        reader = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        for row in reader:
+            yield row
+
+
+tokenlist_file = 'data.definition'
+logger.info("Loading token list from {0}".format(tokenlist_file))
+tokens = LoadTokenList(tokenlist_file)
+logger.info("Loaded {0} tokens".format( len( tokens ) ) )
+
+
+text_object = DrawText("demo text", 500, 1500)
+print text_object
+
+
+#data_file = 'PageReportV2.csv'
+#for row in CsvDataIterator(data_file):
+#    row[4] = HTMLEntitiesToUnicode(BeautifulSoup(row[4]).getText())
+    #logger.debug(row)
+

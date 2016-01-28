dbc710b874e4ce698010c28338a57122f58a2e03
Mat Rowlands <mat.rowlands@justgiving.com>
2016-01-28 16:20:29 +0000

    using real data

--- video.py
+++ video.py
@@ -33,67 +33,59 @@ setDebugLevel(debugLevel)
 
 def LoadTokenList(data_file):
     with open(data_file, 'rb') as f:
-        rows = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-        tokens = rows.next()
-        logger.debug("data definitions :: {0}".format( " :: ".join( map( str, tokens ) ) ) )
-        return tokens
+        reader = csv.reader(f, quotechar='"', delimiter='|', quoting=csv.QUOTE_NONE, skipinitialspace=True)
+        for row in reader:
+			logger.debug("data definitions :: {0}".format( " :: ".join( map( str, row ) ) ) )
+			return row
 
 def CsvDataIterator(data_file):
     with open(data_file, 'rb') as f:
-        reader = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        reader = csv.reader(f, quotechar='"', delimiter='|', quoting=csv.QUOTE_NONE, skipinitialspace=True)
         for row in reader:
             yield row
 
-'''
+
+data_file = 'sample.csv'
 tokenlist_file = 'data.definition'
+template_file = 'template.json.bak'
+skip_headers = True
+max_rows = 5
+
+import json
+logger.info("Loading json template from {0}".format(template_file))
+with open(template_file, 'r') as myfile:
+	template = json.loads(myfile.read().strip())
+logger.info("Loaded json template")
+
 logger.info("Loading token list from {0}".format(tokenlist_file))
 tokens = LoadTokenList(tokenlist_file)
 logger.info("Loaded {0} tokens".format( len( tokens ) ) )
-'''
 
-'''
-movie = FFMPEG("Just Giving 008 Animatic.mov", "outmovie.webm")
+import re
 
-text_object = DrawText("demo text that is getting quite long now and wraps at 30 chars", 100, 500)
-text_object.line_max_length = 30
-text_object.fix_bounds = True
-text_object.font.file = "/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-C.ttf"
-text_object.font.size = 80
-text_object.box.enabled = False
-text_object.box.colour = "0000AA99"
-text_object.box.border_width = 30
-text_object.x = 200
-text_object.y = 700
+# start processing
+if (skip_headers and (max_rows > 0)):
+	max_rows += 1
 
-text_object2 = DrawText('second [escaped] {demo} %text% that is getting quite long :: now and needs more of a test', 300, 600)
-text_object2.line_max_length = 20
-text_object2.fix_bounds = True
-text_object2.font.file = "/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-C.ttf"
-text_object2.font.size = 80
-text_object2.box.enabled = False
-text_object2.box.colour = "FF00AA00"
-text_object2.box.border_width = 30
-text_object2.clean_content = True
-text_object2.x = 600
-text_object2.y = 100
+for row_counter, data_row in enumerate(CsvDataIterator(data_file)):
+    if (len(data_row) > 0):
+		if (skip_headers and (0 == row_counter)):
+		  continue
+		if (max_rows > 0 and max_rows <= row_counter):
+			break
+		logger.info("processing row :: {0}".format(row_counter))
 
-movie.text_objects.append(text_object)
-movie.text_objects.append(text_object2)
+		# swap tokens for data in the template
+		this_script = FFMPEG()
+		this_script.from_JSON(template)
+		for text_object in this_script.text_objects:
+			#print text_object.content
+			for token_counter, token in enumerate(tokens):
+				data = data_row[token_counter]
 
-print (movie)
+				if (re.match(token, text_object.content)):
+					logger.debug("swapping out token :: {0} for data :: '{1}'".format(token, data))
+					text_object.content = re.sub(token, data, text_object.content)
 
-'''
-
-import json
-with open('template.json', 'r') as myfile:
-	template = json.loads(myfile.read())
-	movie = FFMPEG()
-	movie.from_JSON(template)
-	print movie
-
-
-#data_file = 'PageReportV2.csv'
-#for row in CsvDataIterator(data_file):
-#    row[4] = HTMLEntitiesToUnicode(BeautifulSoup(row[4]).getText())
-    #logger.debug(row)
+		print this_script
 

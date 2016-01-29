309b7d840e0b804f25c970cba1e7bcf511e18490
mat rowlands <mat.rowlands@justgiving.com>
2016-01-29 11:26:54 +0000

    app running from initial config

--- video.py
+++ video.py
@@ -13,6 +13,7 @@ import json
 import re
 from FFMPEG import DrawText
 from FFMPEG import FFMPEG
+from Config import Config
 
 # logging setup
 
@@ -33,9 +34,10 @@ setDebugLevel(debugLevel)
 # end logging setup
 
 def LoadTokenList(data_file):
-    with open(data_file, 'rb') as f:
-        reader = csv.reader(f, quotechar='"', delimiter='|', quoting=csv.QUOTE_NONE, skipinitialspace=True)
-        for row in reader:
+	print data_seperator
+	with open(data_file, 'rb') as f:
+		reader = csv.reader(f, quotechar='"', delimiter=str(data_seperator), quoting=csv.QUOTE_NONE, skipinitialspace=True)
+		for row in reader:
 			# add the cwd token
 			row.append(CWD_TOKEN)
 			logger.debug("data definitions :: {0}".format( " :: ".join( map( str, row ) ) ) )
@@ -43,7 +45,7 @@ def LoadTokenList(data_file):
 
 def CsvDataIterator(data_file):
 	with open(data_file, 'rb') as f:
-		reader = csv.reader(f, quotechar='"', delimiter='|', quoting=csv.QUOTE_NONE, skipinitialspace=True)
+		reader = csv.reader(f, quotechar='"', delimiter=str(data_seperator), quoting=csv.QUOTE_NONE, skipinitialspace=True)
 		for row in reader:
 			# add the cwd value
 			row.append(cwd)
@@ -60,13 +62,26 @@ def swap_tokens(tokens, data_row, content):
 
 	return content
 
-data_file = 'sample.csv'
-tokenlist_file = 'data.definition'
-template_file = 'template.json.bak'
-skip_headers = True
-max_rows = 5
+
 cwd = os.getcwd()
 CWD_TOKEN = "%_CWD_%"
+local_materials = "{0}/materials".format(cwd)
+tokenlist_file = "{0}/data.definition".format(local_materials)
+template_file = "{0}/template.json".format(local_materials)
+config_file = "{0}/config.json".format(local_materials)
+
+'''
+config loading starts
+'''
+config = Config()
+logger.info("Loading json config from {0}".format(config_file))
+with open(config_file, 'r') as myfile:
+	config.from_JSON(json.loads(myfile.read().strip()))
+logger.info("Loaded json config")
+skip_headers = config.data_has_headers
+data_seperator = config.data_seperator
+max_rows = config.max_rows
+data_file = "{0}/{1}".format(local_materials, config.data_file)
 
 logger.info("Loading json template from {0}".format(template_file))
 with open(template_file, 'r') as myfile:
@@ -93,16 +108,23 @@ for row_counter, data_row in enumerate(CsvDataIterator(data_file)):
 		this_script = FFMPEG()
 		this_script.from_JSON(template)
 		
-		# fix input output paths possibly containing tokens
-		this_script.source_movie = swap_tokens(tokens, data_row, this_script.source_movie)
-		this_script.destination_movie = swap_tokens(tokens, data_row, this_script.destination_movie)
-		this_script.snapshot_name = swap_tokens(tokens, data_row, this_script.snapshot_name)
+		if (config.create_movie):
+			logger.info("Creating movie")
+			# fix input output paths possibly containing tokens
+			this_script.source_movie = swap_tokens(tokens, data_row, this_script.source_movie)
+			this_script.destination_movie = swap_tokens(tokens, data_row, this_script.destination_movie)
+			this_script.snapshot_name = swap_tokens(tokens, data_row, this_script.snapshot_name)
 
-		# swap tokens for data in the template
-		for text_object in this_script.text_objects:
-			text_object.font.file = re.sub(CWD_TOKEN, cwd, text_object.font.file)
-			text_object.content = swap_tokens(tokens, data_row, text_object.content)
+			# swap tokens for data in the template
+			for text_object in this_script.text_objects:
+				text_object.font.file = re.sub(CWD_TOKEN, cwd, text_object.font.file)
+				text_object.content = swap_tokens(tokens, data_row, text_object.content)
 
-		# run the command
-		print this_script
+			# run the command
+			#print this_script
+
+		if (config.create_html):
+			logger.info("Creating html")
+
+logger.info("Run completed")
 

231964283bb6a1c82929cfcc75431f0aea54b8f0
mat rowlands <mat.rowlands@justgiving.com>
2016-01-29 09:28:03 +0000

    more token swapping for file paths etc

--- video.py
+++ video.py
@@ -36,15 +36,29 @@ def LoadTokenList(data_file):
     with open(data_file, 'rb') as f:
         reader = csv.reader(f, quotechar='"', delimiter='|', quoting=csv.QUOTE_NONE, skipinitialspace=True)
         for row in reader:
+			# add the cwd token
+			row.append(CWD_TOKEN)
 			logger.debug("data definitions :: {0}".format( " :: ".join( map( str, row ) ) ) )
 			return row
 
 def CsvDataIterator(data_file):
-    with open(data_file, 'rb') as f:
-        reader = csv.reader(f, quotechar='"', delimiter='|', quoting=csv.QUOTE_NONE, skipinitialspace=True)
-        for row in reader:
-            yield row
+	with open(data_file, 'rb') as f:
+		reader = csv.reader(f, quotechar='"', delimiter='|', quoting=csv.QUOTE_NONE, skipinitialspace=True)
+		for row in reader:
+			# add the cwd value
+			row.append(cwd)
+			yield row
 
+def swap_tokens(tokens, data_row, content):
+
+	for token_counter, token in enumerate(tokens):
+		data = data_row[token_counter]
+
+		if (re.match(token, content)):
+			logger.debug("swapping out token :: {0} for data :: '{1}'".format(token, data))
+			content = re.sub(token, data, content)
+
+	return content
 
 data_file = 'sample.csv'
 tokenlist_file = 'data.definition'
@@ -79,16 +93,16 @@ for row_counter, data_row in enumerate(CsvDataIterator(data_file)):
 		this_script = FFMPEG()
 		this_script.from_JSON(template)
 		
+		# fix input output paths possibly containing tokens
+		this_script.source_movie = swap_tokens(tokens, data_row, this_script.source_movie)
+		this_script.destination_movie = swap_tokens(tokens, data_row, this_script.destination_movie)
+		this_script.snapshot_name = swap_tokens(tokens, data_row, this_script.snapshot_name)
+
 		# swap tokens for data in the template
 		for text_object in this_script.text_objects:
 			text_object.font.file = re.sub(CWD_TOKEN, cwd, text_object.font.file)
 			#print text_object.content
-			for token_counter, token in enumerate(tokens):
-				data = data_row[token_counter]
-
-				if (re.match(token, text_object.content)):
-					logger.debug("swapping out token :: {0} for data :: '{1}'".format(token, data))
-					text_object.content = re.sub(token, data, text_object.content)
+			text_object.content = swap_tokens(tokens, data_row, text_object.content)
 
 		print this_script
 

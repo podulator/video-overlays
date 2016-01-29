c29fe88915ca1f7c55c0a5155411cf9422b0fff5
mat rowlands <mat.rowlands@justgiving.com>
2016-01-29 11:25:31 +0000

    added initial config work

---
+++ Config/Config.py
@@ -0,0 +1,82 @@
+import json
+
+class Config(object):
+
+	@property
+	def create_html(self):
+		return self._create_html
+	
+	@create_html.setter
+	def create_html(self, value):
+		self._create_html = value
+		
+	@property
+	def create_movie(self):
+		return self._create_movie
+	
+	@create_movie.setter
+	def create_movie(self, value):
+		self._create_movie = value
+
+	@property
+	def data_file(self):
+		return self._data_file
+	
+	@data_file.setter
+	def data_file(self, value):
+		self._data_file = value
+
+	@property
+	def data_has_headers(self):
+		return self._data_has_headers
+	
+	@data_has_headers.setter
+	def data_has_headers(self, value):
+		self._data_has_headers = value
+
+	@property
+	def data_seperator(self):
+		return self._data_seperator
+	
+	@data_seperator.setter
+	def data_seperator(self, value):
+		self._data_seperator = value
+
+	@property
+	def max_rows(self):
+		return self._max_rows
+	
+	@max_rows.setter
+	def max_rows(self, value):
+		self._max_rows = value
+
+	@property
+	def terminate_on_completion(self):
+		return self._terminate_on_completion
+	
+	@terminate_on_completion.setter
+	def terminate_on_completion(self, value):
+		self._terminate_on_completion = value
+
+	def to_JSON(self):
+		return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+	def from_JSON(self, data):
+		self._create_html = data["_create_html"]
+		self._create_movie = data["_create_movie"]
+		self._data_file = data["_data_file"]
+		self._data_seperator = data["_data_seperator"]
+		self._data_has_headers = data["_data_has_headers"]
+		self._max_rows = data["_max_rows"]
+		self._terminate_on_completion = data["_terminate_on_completion"]
+
+	def __init__(self):
+		self._create_html = True
+		self._create_movie = True
+		self._data_file = ""
+		self._data_seperator = ","
+		self._data_has_headers = False
+		self._max_rows = 0
+		self._terminate_on_completion = False
+
+

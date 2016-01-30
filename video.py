1427c00465798d8d5936a663c92caecb3eed5c58
mat rowlands <mat.rowlands@justgiving.com>
2016-01-30 10:50:47 +0000

    logging, args handling and log uploading

--- video.py
+++ video.py
@@ -5,37 +5,113 @@
 # pip install boto BeautifulSoup
 
 import csv
-import boto
+import boto.utils
+import boto.ec2
 from boto.s3.connection import S3Connection
 import logging
 import sys
 import os
 import json
 import re
+from socket import gethostname
+import datetime
 from FFMPEG import DrawText
 from FFMPEG import FFMPEG
 from Config import Config
 
-# logging setup
-
-logger = logging.getLogger('Video Processor')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch = logging.StreamHandler(sys.stdout)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
+s3_materials_path = ""
+s3_logs_path = ""
+cwd = os.getcwd()
+CWD_TOKEN = "%_CWD_%"
+local_materials = "{0}/materials".format(cwd)
+tokenlist_file = "{0}/data.definition".format(local_materials)
+template_file = "{0}/template.json".format(local_materials)
+config_file = "{0}/config.json".format(local_materials)
+log_file = "{}-{}.log".format( gethostname(), datetime.datetime.now().isoformat(' ') )
+log_file = log_file.replace(" ", "-")
+    
 def setDebugLevel(val):
-    logger.setLevel(val)
-    ch.setLevel(val)
+	logger.setLevel(val)
+	log_console.setLevel(val)
+	log_file_handler.setLevel(val)
 
-debugLevel = logging.INFO
-debugLevel = logging.DEBUG
-setDebugLevel(debugLevel)
+def show_help():
+	path = sys.argv[0]
+	print "run as {0} [ -help or -local or S3MaterialsURL or S3MaterialsURL:S3LogsURL ]".format(path)
+	print "-help :: show this help"
+	print "-local :: assume all materials alreaady exist under ./materials/"
+	print "S3MaterialsURL :: The full bucket name and path to a materials forlder. Logs will be stored in derived bucketname/video_logs"
+	print "S3MaterialsURL:S3LogsURL :: Full bucket name and path for both materials and logs, colon seperated"
+	print ""
 
-# end logging setup
+def get_bucket_and_path_from_s3_url(url):
+	url_parts = url.split("/")
+	# get the path minus the bucket name
+	bucket = url_parts[0]
+	path_root = "/".join(url_parts[1:])
+	# always force a trailing slash if it's not an empty string
+	if ("" != path_root):
+		if ("/" != path_root[-1]):
+			path_root = "{0}/".format(path_root)
+			logger.debug("Path normalised to :: {0}".format(path_root))
+	
+	logger.debug("Bucket == {0} :: Path = {1}".format(bucket, path_root))
+	return bucket, path_root
+
+def upload_file_to_S3(source, destination):
+	try:
+		logger.info("Uploading {0} to {1}".format(source, destination))
+		bucket_name, path = get_bucket_and_path_from_s3_url(destination)
+		# strip trailing slash
+		path = path[:-1]
+		logger.debug("Connecting to bucket :: {}".format(bucket_name))
+		conn = S3Connection()
+		bucket = conn.get_bucket(bucket_name)
+		logger.debug("Setting key :: {0}".format(path))
+		key = boto.s3.key.Key(bucket)
+		key.key = path
+		key.set_contents_from_filename(source)
+		logger.info("Upload complete")
+		return True
+	except Exception as e:
+		logger.error(e)
+		return False
+
+# sort out the start up params from the cli or fall back to user data
+def handle_startup_params():
+	global s3_materials_path, s3_logs_path
+	params = ""
+	if (len(sys.argv) > 1):
+		params = sys.argv[1]
+		logger.debug("Got startup params :: {0}".format(params))
+	else:
+		logger.info("Trying to get startup params from user data")
+		params = boto.utils.get_instance_userdata()
+	
+	params = params.strip()
+	if (len(params) == 0):
+		show_help()
+		exit(1)
+	else:
+		if (params == "-help"):
+			show_help()
+			exit(0)
+		elif (params == "-local"):
+			pass
+		else:
+			param_parts = params.split(":")
+			if (len(param_parts) >= 2):
+				s3_materials_path = param_parts[0]
+				s3_logs_path = param_parts[1]
+			else:
+				s3_materials_path = param_parts[0]
+				s3_logs_bucket, discard_path = get_bucket_and_path_from_s3_url(s3_materials_path)
+				s3_logs_path = "{0}/video_logs/".format(s3_logs_bucket)
+	logger.debug("s3_materials_path = {0}".format(s3_materials_path))
+	logger.debug("s3_logs_path = {0}".format(s3_logs_path))
 
 def LoadTokenList(data_file):
-	print data_seperator
+	global data_seperator
 	with open(data_file, 'rb') as f:
 		reader = csv.reader(f, quotechar='"', delimiter=str(data_seperator), quoting=csv.QUOTE_ALL, skipinitialspace=True)
 		for row in reader:
@@ -45,6 +121,7 @@ def LoadTokenList(data_file):
 			return row
 
 def CsvDataIterator(data_file):
+	global data_seperator
 	with open(data_file, 'rb') as f:
 		reader = csv.reader(f, quotechar='"', delimiter=str(data_seperator), quoting=csv.QUOTE_NONE, skipinitialspace=True)
 		for row in reader:
@@ -63,31 +140,41 @@ def swap_tokens(tokens, data_row, content):
 
 	return content
 
+# logging setup
 
-cwd = os.getcwd()
-CWD_TOKEN = "%_CWD_%"
-local_materials = "{0}/materials".format(cwd)
-tokenlist_file = "{0}/data.definition".format(local_materials)
-template_file = "{0}/template.json".format(local_materials)
-config_file = "{0}/config.json".format(local_materials)
+if (os.path.exists("*.log")):
+	os.remove("*.log")
+
+logger = logging.getLogger('Video Processor')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log_console = logging.StreamHandler(sys.stdout)
+log_console.setFormatter(formatter)
+logger.addHandler(log_console)
+
+log_file_handler = logging.FileHandler(log_file)
+log_file_handler.setFormatter(formatter)
+logger.addHandler(log_file_handler)
+
+setDebugLevel(logging.INFO)
+
+# end logging setup
+
+logger.info("Run started")
 
 '''
 fetch materials?
 '''
+
 try:
+
+	handle_startup_params()
+
 	if not os.path.exists(local_materials):
 		os.mkdir(local_materials)
-	if (len(sys.argv) > 1):
-		path_parts = sys.argv[1].split("/")
-		remote_bucket = path_parts[0]
-		# get the path minus the bucket name
-		remote_path_root = "/".join(path_parts[1:])
-		# always force a trailing slash if it's not an empty string
-		if ("" != remote_path_root):
-			if ("/" != remote_path_root[-1]):
-				remote_path_root = "{0}/".format(remote_path_root)
-				logger.debug("Remote path normalised to :: {0}".format(remote_path_root))
 
+	if (len(s3_materials_path) > 0):
+		
+		remote_bucket, remote_path_root = get_bucket_and_path_from_s3_url(s3_materials_path)
 		remote_path_root_length = len(remote_path_root)
 
 		logger.info("Downloading materials from bucket :: {0}".format(remote_bucket))
@@ -99,8 +186,8 @@ try:
 			local_path = "{0}/{1}".format(local_materials, remote_key[remote_path_root_length:])
 			logger.info("Downloading {0} to {1}".format(remote_key, local_path))
 			try:
-				item.get_contents_to_filename(local_path)
-				#pass
+				#item.get_contents_to_filename(local_path)
+				pass
 			except OSError:
 				if not os.path.exists(local_path):
 					logger.info("Creating local folder :: {0}".format(local_path))
@@ -110,11 +197,9 @@ try:
 	else:
 		logger.info("No materials bucket supplied, assuming everything is already local")
 		
-except Exception as e:
+except ValueError as e:
 	logger.error("Couldn't fetch materials :: {0}".format(e))
 	exit(1)
-
-exit(0)
 
 '''
 config loading starts
@@ -189,4 +274,4 @@ for row_counter, data_row in enumerate(CsvDataIterator(data_file)):
 			logger.info("Creating html")
 
 logger.info("Run completed")
-
+upload_file_to_S3(log_file, "{0}/{1}".format(s3_logs_path, log_file))
